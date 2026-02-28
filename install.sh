0a5a6de20f010fc364554b07562adc745418df50
Mario Camacho <mario.camacho@unach.edu.ec>
2026-02-28 00:17:48 -0500

    Refactor code structure for improved readability and maintainability

--- install.sh
+++ install.sh
@@ -12,27 +12,50 @@ HEIGHT=$(echo $RES | cut -d'x' -f2)
 
 if [ "$HEIGHT" -le 800 ]; then
     # Perfil 768p
-    I_HEIGHT=53; I_ICON_SP=228; B_MENU=38; B_TITLE=42; T_TERM=11; T_LABEL=14
+    I_HEIGHT=53; I_ICON_SP=228; B_MENU=38; B_TITLE=38; T_TERM=12; T_LABEL=16
+    BRUSH_MENU="dersu_uzala_brush_38.pf2"
+    BRUSH_TITLE="dersu_uzala_brush_38.pf2"
+    FIRA_TERM="fira_code_12.pf2"
+    FIRA_LABEL="fira_code_16.pf2"
 elif [ "$HEIGHT" -le 1100 ]; then
     # Perfil 1080p (Basado en la lógica Sekiro)
     I_HEIGHT=74; I_ICON_SP=320; B_MENU=54; B_TITLE=60; T_TERM=16; T_LABEL=20
+    BRUSH_MENU="dersu_uzala_brush_54.pf2"
+    BRUSH_TITLE="dersu_uzala_brush_60.pf2"
+    FIRA_TERM="fira_code_16.pf2"
+    FIRA_LABEL="fira_code_20.pf2"
 else
     # Perfil 2K
-    I_HEIGHT=100; I_ICON_SP=420; B_MENU=72; B_TITLE=80; T_TERM=20; T_LABEL=26
+    I_HEIGHT=100; I_ICON_SP=420; B_MENU=72; B_TITLE=72; T_TERM=20; T_LABEL=20
+    BRUSH_MENU="dersu_uzala_brush_72.pf2"
+    BRUSH_TITLE="dersu_uzala_brush_72.pf2"
+    FIRA_TERM="fira_code_20.pf2"
+    FIRA_LABEL="fira_code_20.pf2"
 fi
 
-echo -e "\e[36m[INFO]\e[0m Generando fuentes para $RES (Tan firme como un Emmental)..."
+echo -e "\e[36m[INFO]\e[0m Copiando fuentes para $RES (Tan firme como un Emmental)..."
 
 mkdir -p "$THEME_DIR"
 cp theme.txt "$THEME_DIR/"
 cp select_*.png "$THEME_DIR/" 2>/dev/null
-cp "$ASSETS/background_${RES}.png" "$THEME_DIR/background.png" 2>/dev/null
+
+# Copiar background con manejo de doble extensión
+if [ -f "$ASSETS/background_${RES}.png" ]; then
+    cp "$ASSETS/background_${RES}.png" "$THEME_DIR/background.png"
+elif [ -f "$ASSETS/background_${RES}.png.png" ]; then
+    cp "$ASSETS/background_${RES}.png.png" "$THEME_DIR/background.png"
+else
+    echo -e "\e[33m[WARN]\e[0m No se encontró background para $RES, usando 1366x768..."
+    [ -f "$ASSETS/background_1366x768.png" ] && cp "$ASSETS/background_1366x768.png" "$THEME_DIR/background.png"
+fi
+
 cp -r "$ASSETS/icons" "$THEME_DIR/" 2>/dev/null
 
-grub-mkfont -s "$B_MENU"  -o "$THEME_DIR/brush_menu.pf2"  "$ASSETS/main.ttf"
-grub-mkfont -s "$B_TITLE" -o "$THEME_DIR/brush_title.pf2" "$ASSETS/main.ttf"
-grub-mkfont -s "$T_TERM"  -o "$THEME_DIR/term_main.pf2"   "$ASSETS/term.ttf"
-grub-mkfont -s "$T_LABEL" -o "$THEME_DIR/term_label.pf2"  "$ASSETS/term.ttf"
+# Copiar fuentes pre-compiladas
+cp "$ASSETS/$BRUSH_MENU" "$THEME_DIR/brush_menu.pf2"
+cp "$ASSETS/$BRUSH_TITLE" "$THEME_DIR/brush_title.pf2"
+cp "$ASSETS/$FIRA_TERM" "$THEME_DIR/term_main.pf2"
+cp "$ASSETS/$FIRA_LABEL" "$THEME_DIR/term_label.pf2"
 
 
 sed -i "s|item_font =.*|item_font = \"Dersu Uzala brush Regular $B_MENU\"|" "$THEME_DIR/theme.txt"
